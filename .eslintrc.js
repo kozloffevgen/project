5ba17a3d04f869328aa773234b37eaf13cd6cbb0
Evgen <inevitability.k@gmail.com>
2024-01-05 12:36:50 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    'vue/v-on-event-hyphenation': 'error',
+    'vue/require-explicit-emits': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+    "vue/component-name-in-template-casing": ['error', "kebab-case"],
+  }
+}
