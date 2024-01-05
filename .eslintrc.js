f9a88e8e2607bc6d00d95575ea5089ad85b6f192
Evgen <inevitability.k@gmail.com>
2024-01-05 13:31:30 +0300

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,6 @@ module.exports = {
     'vue/require-explicit-emits': 'error',
     'vue/order-in-components': 'error',
     'vue/this-in-template': 'error',
-    "vue/component-name-in-template-casing": ['error', "kebab-case"],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   }
 }
