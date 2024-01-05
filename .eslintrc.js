4498c6a2ba087a9e34cb9ba433280ac95e58ce3c
Evgen <inevitability.k@gmail.com>
2024-01-05 13:49:49 +0300

    Eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     'vue/require-explicit-emits': 'error',
     'vue/order-in-components': 'error',
     'vue/this-in-template': 'error',
+    'vue/no-multi-spaces': 'error',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'error',
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   }
 }
